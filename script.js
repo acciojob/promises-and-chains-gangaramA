8e806da968e39058c9c33a930e392b5b963e1d0f
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-12-23 17:35:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+let age=document.queryselector("#age")
+let name=document.queryselector("#name")
+let btn=document.queryselector("#btn")
+
+age.addEventListener("click", (event)=>{
+
+	const agePromise=new Promese((resolve,reject)=>{
+
+		setTimeOut(()=>{
+			
+		if(parseInt(age)>18){
+			resolve(`Welcome, ${name}. You can vote`)
+		}
+
+		else{
+			reject(`Oh sorry ${name}. You aren't old enough.`)
+		}
+		},4000)
+
+	})
+
+	
+    agePromise
+        .then((message) => alert(message))
+        .catch((error) => alert(error));
+	
+})
+
+
